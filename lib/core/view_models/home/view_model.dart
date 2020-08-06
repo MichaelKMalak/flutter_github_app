4f62eeb1e6af7becc926ca7c5ca8e21cf0b8d226
Michael <michael.k.malak@gmail.com>
2020-08-07 01:23:16 +0200

    Optimize imports in HomeViewModel

--- lib/core/view_models/home/view_model.dart
+++ lib/core/view_models/home/view_model.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/animation.dart';
-import 'package:flutter/foundation.dart';
-// ignore: implementation_imports
-import 'package:flutter/src/widgets/page_view.dart';
+import 'package:flutter/material.dart';
 import 'package:mobile_code_challenge_solution/core/providers/controller/provider.dart';
 import 'package:mobile_code_challenge_solution/core/view_models/base.dart';
 
