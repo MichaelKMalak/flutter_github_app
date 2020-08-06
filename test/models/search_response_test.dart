51d497b2f2038e34cf7c8e30228b52ec622bfca2
12827629 <michael.k.malak@gmail.com>
2020-08-06 16:59:49 +0200

    Update search response unit test to allow for parsing in the background

--- test/models/search_response_test.dart
+++ test/models/search_response_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobile_code_challenge_solution/core/models/search_response/search_response.dart';
 
@@ -11,14 +12,16 @@ void main() {
 
       //mocking the mobile side
       final searchResponseModel =
-          SearchResponse.fromJsonStr(server.responseJsonStr);
+          compute(SearchResponse.fromJsonStr, server.responseJsonStr);
 
-      expect(searchResponseModel.items.first.name,
-          equals(server.repositoryJsonObj['name']));
-      expect(searchResponseModel.items.first.description,
-          equals(server.repositoryJsonObj['description']));
-      expect(searchResponseModel.items.first.stargazersCount,
-          equals(server.repositoryJsonObj['stargazers_count']));
+      searchResponseModel.then((value) {
+        expect(
+            value.items.first.name, equals(server.repositoryJsonObj['name']));
+        expect(value.items.first.description,
+            equals(server.repositoryJsonObj['description']));
+        expect(value.items.first.stargazersCount,
+            equals(server.repositoryJsonObj['stargazers_count']));
+      });
     });
   });
 }
