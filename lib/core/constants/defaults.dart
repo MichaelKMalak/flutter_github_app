97a3e83f42c54642343e345853f994d356d9dc8a
12827629 <michael.k.malak@gmail.com>
2020-08-07 13:16:27 +0200

    Refactor defaults in core/constants

---
+++ lib/core/constants/defaults.dart
@@ -0,0 +1,4 @@
+import 'package:mobile_code_challenge_solution/core/models/sort_type/sort_type.dart';
+
+const int numOfDaysAgoByDefault = 30;
+const SortType sortTypeByDefault = SortType.desc;
--- lib/core/view_models/splash/view_model.dart
+++ lib/core/view_models/splash/view_model.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:mobile_code_challenge_solution/core/constants/defaults.dart';
 import 'package:mobile_code_challenge_solution/core/constants/routes.dart';
 import 'package:mobile_code_challenge_solution/core/models/search_filter/search_filter.dart';
-import 'package:mobile_code_challenge_solution/core/models/sort_type/sort_type.dart';
 import 'package:mobile_code_challenge_solution/core/providers/get_repos/provider.dart';
 import 'package:mobile_code_challenge_solution/core/view_models/base.dart';
 
@@ -14,8 +14,8 @@ class SplashViewModel extends BaseModel {
 
   Future<void> onReady(BuildContext context) async {
     final defaultSearchFilter = SearchFilter((SearchFilterBuilder b) => b
-      ..numOfDaysAgo = 30
-      ..sortType = SortType.desc);
+      ..numOfDaysAgo = numOfDaysAgoByDefault
+      ..sortType = sortTypeByDefault);
 
     setBusy(true);
     await _repositoriesProvider.getRepositories(defaultSearchFilter);
