82e2c16296f837e7fbaab154b993d28d75d3cc3d
12827629 <michael.k.malak@gmail.com>
2020-08-07 15:07:47 +0200

    Cleanup unused imports in serializers.dart

--- lib/core/models/serializers.dart
+++ lib/core/models/serializers.dart
@@ -3,10 +3,8 @@ library serializers;
 import 'package:built_collection/built_collection.dart';
 import 'package:built_value/serializer.dart';
 import 'package:built_value/standard_json_plugin.dart';
-import 'package:mobile_code_challenge_solution/core/models/order_type/order_type.dart';
 import 'package:mobile_code_challenge_solution/core/models/repository/repository.dart';
 import 'package:mobile_code_challenge_solution/core/models/repository_owner/repository_owner.dart';
-import 'package:mobile_code_challenge_solution/core/models/search_filter/search_filter.dart';
 import 'package:mobile_code_challenge_solution/core/models/search_response/search_response.dart';
 
 part 'serializers.g.dart';
