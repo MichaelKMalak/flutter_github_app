262ed56d3b180ee75981d6bf7f071912730f8c2a
12827629 <michael.k.malak@gmail.com>
2020-08-09 20:28:29 +0200

    Switch between web mode and phone mode by checking dimensions

--- lib/ui/router.dart
+++ lib/ui/router.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/widgets.dart';
 import 'package:mobile_code_challenge_solution/core/constants/routes.dart';
-import 'package:mobile_code_challenge_solution/ui/views/home/view.dart';
+import 'package:mobile_code_challenge_solution/ui/views/home/switcher.dart';
 import 'package:mobile_code_challenge_solution/ui/views/splash/view.dart';
-import 'package:mobile_code_challenge_solution/ui/widgets/web_app_wrapper.dart';
 
 class Router {
   static dynamic routes() => {
     RoutePaths.splash: (BuildContext context) => SplashView(),
-    RoutePaths.home: (BuildContext context) => WebAppWrapper(HomeView()),
+    RoutePaths.home: (BuildContext context) => HomeSwitcher(),
   };
 }
